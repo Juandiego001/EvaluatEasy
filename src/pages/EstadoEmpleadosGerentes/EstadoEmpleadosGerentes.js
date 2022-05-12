0fc56bf404eab857807d21419bea6cd4ed807c41
Usuario ILINF <admin-ilinf@uao.edu.co>
2022-05-12 18:21:39 -0500

    Agregando la interfaz de EstadoEmpleados para Gerentes

---
+++ src/pages/EstadoEmpleadosGerentes/EstadoEmpleadosGerentes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './EstadoEmpleadosGerentes.module.css';
+
+const EstadoEmpleadosGerentes = () => (
+  <div className={styles.EstadoEmpleadosGerentes}>
+    EstadoEmpleadosGerentes Component
+  </div>
+);
+
+EstadoEmpleadosGerentes.propTypes = {};
+
+EstadoEmpleadosGerentes.defaultProps = {};
+
+export default EstadoEmpleadosGerentes;
