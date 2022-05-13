1c3a9b31795b1692b64f9fbd8372696159f4c1ce
Usuario ILINF <admin-ilinf@uao.edu.co>
2022-05-13 16:21:15 -0500

    Creando service de EstadoEmpleadosGerentesService y el componente para poder determinar el estado de los empleados cuando acceda a la interfaz

--- src/pages/EstadoEmpleadosGerentes/EstadoEmpleadosGerentes.js
+++ src/pages/EstadoEmpleadosGerentes/EstadoEmpleadosGerentes.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './EstadoEmpleadosGerentes.module.css';
+import EstadoEmpleadosGerentesService from '../../services/EstadoEmpleadosGerentesService';
 
-const EstadoEmpleadosGerentes = () => (
-  <div className={styles.EstadoEmpleadosGerentes}>
-    EstadoEmpleadosGerentes Component
-  </div>
-);
+const EstadoEmpleadosGerentes = (props) => {
+
+  const location = useLocation();
+
+  useEffect(() => {
+      
+  });
+
+  return (
+    <div className={styles.EstadoEmpleadosGerentes}>
+      {/* Aquí debe insertarse el Menu */}
+      {/* El menu lo tengo en el computador personal (Cobo). A penas pueda hacer git push pongo el menú */}
+
+      
+    </div>
+  )};
 
 EstadoEmpleadosGerentes.propTypes = {};
 
