957f606ef7f3001eb311b110aafd0a86ed1030e7
Juandiego001 <juandiego14012003@gmail.com>
2022-05-19 06:16:13 -0500

    Cambios en la base de datos (modelado y script)

--- database/creacion_bd.sql
+++ database/creacion_bd.sql
@@ -15,6 +15,43 @@ CREATE TABLE USUARIOS(
     estado INTEGER NULL
 );
 
+CREATE TABLE COEVALUACIONES(
+    id INTEGER NOT NULL AUTO INCREMENT,
+    id_usuario INTEGER NOT NULL,
+    fechaAsignacion DATE NOT NULL,
+    fechaFinalizacion DATE NULL,
+    CONSTRAINT fK_users1 FOREIGN KEY id_usuario REFERENCES FROM USUARIOS(id)
+);
+
+
+CREATE TABLE EVALUACIONES(
+id INTEGER NOT NULL AUTO INCREMENT,
+    id_usuario INTEGER NOT NULL,
+    fechaAsignacion DATE NOT NULL,
+    fechaFinalizacion DATE NULL,
+    CONSTRAINT fK_users2 FOREIGN KEY id_usuario REFERENCES FROM USUARIOS(id)
+);
+
+CREATE TABLE COMPETENCIAS(
+    
+);
+
+CREATE TABLE VALORACIONES(
+    id INTEGER NOT NULL AUTO INCREMENT,
+    letra VARCHAR(1) NULL DEFAULT 'A',
+    id_competencia INTEGER NOT NULL,
+    CONSTRAINT fk_competencias1 FOREIGN KEY id_competencia REFERENCES FROM COMPETENCIAS(id)
+);
+
+CREATE TABLE VALORACIONES_FINALES(
+    id INTEGER NOT NULL AUTO INCREMENT,
+    deTrabajador VARCHAR(1) NULL DEFAULT 'A',
+    deGerente VARCHAR(1) NULL DEFAULT 'A',
+    id_competencia INTEGER NOT NULL,
+    CONSTRAINT fk_competencias2 FOREIGN KEY id_competencia REFERENCES FROM COMPETENCIAS(id)
+);
+
+
 -- Si no quieren crear de nuevo todas las tablas, a continuación algunos ALTER TABLE que programé
 ALTER TABLE USUARIOS MODIFY COLUMN estado INTEGER NULL DEFAULT 1; 
 
