b8b99ae54b32744f50dc632350bf32f7894c5f73
juanfelipesantamaria2901 <71913511+juanfelipesantamaria2901@users.noreply.github.com>
2022-05-18 19:09:35 -0500

    Nueva version evaluaciones

--- src/pages/Evaluaciones/Evaluaciones.js
+++ src/pages/Evaluaciones/Evaluaciones.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import styles from './Evaluaciones.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import { Chart } from 'react-google-charts';
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
@@ -68,29 +67,183 @@ const Evaluaciones = () => {
       </Navbar>
 
       <div className='container'>
-        <Fragment>
-      <div className="form-group">
-        <label htmlFor="example2">Medium input</label>
-        <input type="text" id="example2" className="form-control form-control-sm" />
+        <div className='card'>
+          <div className='card-header'>
+            <h3>Evaluaciones</h3>
+          </div>
+          <div className='card-body'>
+            <Fragment>
+            <div className="form-group">
+              <div className='card-header'>
+              <label htmlFor="example2">Es la habilidad para participar conjunta, organizada y activamente en el logro de objetivos y metas comunes, 
+                alineados a las estrategias de la organización, requiere facilidad para las relaciones interpersonales y capacidad para lograr sinergia 
+                en el éxito de las labores del equipo. Colabora más allá del equipo para obtener resultados, crea y mantiene la confianza, promueve la inclusión
+                 y el respeto por los demás. </label>
+                 </div>
+              <div className='card-body'>
+                <select className="form-select" aria-label="Default select example">
+                  <option selected>Seleccione la calificación</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+              </div>
+              <div className="form-group">
+              <div className='card-header'>
+                <label htmlFor="example2">Supera las metas con éxito. Entrega resultados con un alto nivel de integridad, se enfoca en la ejecución,
+                 monitorea resultados, asume nuevos retos, demuestra responsabilidad, toma decisiones, elimina barreras. 
+                 Se mantiene concentrado en los objetivos a alcanzar. Consigue resultados sobresalientes.</label>
+                 </div>
+                 <div className='card-body'>
+                <select className="form-select" aria-label="Default select example">
+                  <option selected>Seleccione la calificación</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+                </div>
+              </div>
+              <div className="form-group">
+              <div className='card-header'>
+                <label htmlFor="example2">Capacidad de escuchar, hacer preguntas y expresar conceptos e ideas de forma clara, concreta y oportuna, 
+                para asegurar una transmisión de información efectiva a traves de la escritura y el lenguaje verbal y no verbal</label>
+                 </div>
+                 <div className='card-body'>
+                <select className="form-select" aria-label="Default select example">
+                  <option selected>Seleccione la calificación</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+                </div>
+              </div>
+              <div className="form-group">
+              <div className='card-header'>
+                <label htmlFor="example2">Se esfuerza por conocer y resolver las necesidades de los clientes,
+                  demuestra iniciativas para desarrollar excelentes relaciones con clientes internos y externos.
+                  Logra la satisfacción de las necesidades del cliente de forma acertada y oportuna ofreciendo valor
+                  agregado y un balance costo-beneficio.</label>
+                  </div>
+                  <div className='card-body'>
+                <select className="form-select" aria-label="Default select example">
+                  <option selected>Seleccione la calificación</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+                </div>
+              </div>
+              <div className="form-group">
+              <div className='card-header'>
+              <label htmlFor="example2">Es la habilidad para participar conjunta, organizada y activamente en el logro de objetivos y metas comunes, 
+                alineados a las estrategias de la organización, requiere facilidad para las relaciones interpersonales y capacidad para lograr sinergia 
+                en el éxito de las labores del equipo. Colabora más allá del equipo para obtener resultados, crea y mantiene la confianza, promueve la inclusión
+                 y el respeto por los demás. </label>
+                 </div>
+              <div className='card-body'>
+                <select className="form-select" aria-label="Default select example">
+                  <option selected>Seleccione la calificación</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+              </div>
+              <div className="form-group">
+              <div className='card-header'>
+              <label htmlFor="example2">Capacidad para identificar y comprender rápidamente los cambios en el entorno de la organización, tanto interno como externo;
+               transformar las debilidades  en fortalezas y potenciar  estas últimas a través  de planes de acción tendientes a  asegurar  en el largo plazo la presencia y 
+               el posicionamiento de la organización y la consecución de las metas deseadas.</label>
+                 </div>
+              <div className='card-body'>
+                <select className="form-select" aria-label="Default select example">
+                  <option selected>Seleccione la calificación</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+              </div>
+              <div className="form-group">
+              <div className='card-header'>
+              <label htmlFor="example2">Es actuar para reducir al máximo la incertidumbre en su entorno. Se refleja en la continua comprobación y control del trabajo,
+               información o la insistencia para que los roles y las funciones asignadas estén claras. Establece y mantiene sistemas de información.. </label>
+                 </div>
+              <div className='card-body'>
+                <select className="form-select" aria-label="Default select example">
+                  <option selected>Seleccione la calificación</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+              </div>
+              <div className="form-group">
+              <div className='card-header'>
+              <label htmlFor="example2">Claridad para fijar objetivos, prioridades y comunicarlos. Energía e interés por nuevos retos.  Influencia en los colaboradores
+               para el logro de metas, dándoles confianza y facultándolos.  Habilidad para coordinar efectivamente a sus colaboradores.. </label>
+                 </div>
+              <div className='card-body'>
+                <select className="form-select" aria-label="Default select example">
+                  <option selected>Seleccione la calificación</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+              </div>
+              <div className="form-group">
+              <div className='card-header'>
+              <label htmlFor="example2">Habilidad para comprender rapidamente los cambios del entorno,  oportunidades del mercado, amenazas competitivas,
+               fortalezas y debilidades de su área a la hora de identificar una optima respuesta estrategica para la organización. </label>
+                 </div>
+              <div className='card-body'>
+                <select className="form-select" aria-label="Default select example">
+                  <option selected>Seleccione la calificación</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+              </div>
+              <div className="form-group">
+              <div className='card-header'>
+              <label htmlFor="example2">Habilidad para responder en forma oportuna y adecuada ante los problemas y situaciones del ámbito laboral y 
+              de las interacciones personales, utilizando sus propios criterios, después de analizar y profundizar en los asuntos para tener un diagnóstico 
+              objetivo que sirva como base para la solución de problemas y decisiones, utilizando el juicio y la sensatez de razonamiento. </label>
+                 </div>
+              <div className='card-body'>
+                <select className="form-select" aria-label="Default select example">
+                  <option selected>Seleccione la calificación</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                </select>
+              </div>
+              </div>
+              <div className="form-group">
+              <div className='card-header'>
+                <label htmlFor="example3">Observaciones privadas</label>
+                </div>
+                <div className='card-body'>
+                <MDBInput type="textarea" />
+                </div>
+              </div>
+            </Fragment>
+          </div>
+        </div>
       </div>
-      <div className="form-group">
-        <label htmlFor="example2">Medium input</label>
-        <input type="text" id="example2" className="form-control form-control-sm" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="example2">Medium input</label>
-        <input type="text" id="example2" className="form-control form-control-sm" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="example3">Se esfuerza por conocer y resolver las necesidades de los clientes, <br/>
-        demuestra iniciativas para desarrollar excelentes  relaciones con clientes internos y externos.<br/>
-         Logra la satisfacción de las necesidades del cliente de forma acertada y oportuna ofreciendo valor <br/>
-         agregado y un balance costo-beneficio.</label>
-      <MDBInput type="textarea" />
-      </div>
-    </Fragment>
-      </div>
-
 
     </div>
   );
